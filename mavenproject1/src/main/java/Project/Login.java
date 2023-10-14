d57725e22d6dd99845faddcf8b0f761184a3c0ce
jeslinpjames <jeslinpjames007@gmail.com>
2023-10-15 00:05:34 +0530

    added main function

--- mavenproject1/src/main/java/Project/Login.java
+++ mavenproject1/src/main/java/Project/Login.java
@@ -8,6 +8,114 @@ package Project;
  *
  * @author jeslin
  */
-public class NewClass {
-    
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends JFrame implements ActionListener{
+    JLabel l1,l2,l3;
+    JTextField tf1;
+    JPasswordField pf2;
+    JButton b1,b2,b3;
+  
+    Login(){
+        setTitle("ATM");
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l11 = new JLabel(i3);
+        l11.setBounds(70, 10, 100, 100);
+        add(l11);
+        
+        l1 = new JLabel("WELCOME TO ATM");
+        l1.setFont(new Font("Osward", Font.BOLD, 38));
+        l1.setBounds(200,40,450,40);
+        add(l1);
+        
+        l2 = new JLabel("Card No:");
+        l2.setFont(new Font("Raleway", Font.BOLD, 28));
+        l2.setBounds(125,150,375,30);
+        add(l2);
+        
+        tf1 = new JTextField(15);
+        tf1.setBounds(300,150,230,30);
+        tf1.setFont(new Font("Arial", Font.BOLD, 14));
+        add(tf1);
+        
+        l3 = new JLabel("PIN:");
+        l3.setFont(new Font("Raleway", Font.BOLD, 28));
+        l3.setBounds(125,220,375,30);
+        add(l3);
+        
+        pf2 = new JPasswordField(15);
+        pf2.setFont(new Font("Arial", Font.BOLD, 14));
+        pf2.setBounds(300,220,230,30);
+        add(pf2);
+                
+        b1 = new JButton("SIGN IN");
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
+        
+        b2 = new JButton("CLEAR");
+        b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
+        
+        b3 = new JButton("SIGN UP");
+        b3.setBackground(Color.BLACK);
+        b3.setForeground(Color.WHITE);
+        
+        setLayout(null);
+        
+        b1.setFont(new Font("Arial", Font.BOLD, 14));
+        b1.setBounds(300,300,100,30);
+        add(b1);
+        
+        b2.setFont(new Font("Arial", Font.BOLD, 14));
+        b2.setBounds(430,300,100,30);
+        add(b2);
+        
+        b3.setFont(new Font("Arial", Font.BOLD, 14));
+        b3.setBounds(300,350,230,30);
+        add(b3);
+        
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+        
+        getContentPane().setBackground(Color.WHITE);
+        
+        setSize(800,480);
+        setLocation(550,200);
+        setVisible(true);
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    public static void main(String[] args){
+        new Login().setVisible(true);
+    }
 }
+    
+
+
